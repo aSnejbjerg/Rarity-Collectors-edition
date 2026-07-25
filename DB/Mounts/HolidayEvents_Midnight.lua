8234bf309a4071e51a99b5067ffbdf34d8732060
Napu <42168095+aSnejbjerg@users.noreply.github.com>
2026-07-25 16:04:33 +0200

    Update HolidayEvents_Midnight.lua
    Replaced spaces with tabs to fix styling errors. (design choice)

--- DB/Mounts/HolidayEvents_Midnight.lua
+++ DB/Mounts/HolidayEvents_Midnight.lua
@@ -9,23 +9,21 @@ end
 
 local holidayEventMountsMidnight = {
 	["Sun Festival's Painted Roc"] = {
-        cat = CONSTANTS.ITEM_CATEGORIES.HOLIDAY,
-        type = CONSTANTS.ITEM_TYPES.MOUNT,
-        method = CONSTANTS.DETECTION_METHODS.USE,
-        name = L["Sun Festival's Painted Roc"],
-        spellId = 1298439,
-        itemId = 275464,
-        items = { 54536, 117394 },
-        chance = 200,
-        groupSize = 5,
-        equalOdds = true,
-        sourceText = L["Can be contained in the Satchel of Chilled Goods, rewarded for defeating the World Event Dungeon during the Midsummer Fire Festival."],
-        lockDungeonId = 286,
+		cat = CONSTANTS.ITEM_CATEGORIES.HOLIDAY,
+		type = CONSTANTS.ITEM_TYPES.MOUNT,
+		method = CONSTANTS.DETECTION_METHODS.USE,	
+		name = L["Sun Festival's Painted Roc"],
+		spellId = 1298439,
+		itemId = 275464,
+		items = { 54536, 117394 },
+		chance = 200,
+		groupSize = 5,
+		equalOdds = true,
+		sourceText = L["Can be contained in the Satchel of Chilled Goods, rewarded for defeating the World Event Dungeon during the Midsummer Fire Festival."],
+		lockDungeonId = 286,
 		holidayEvents = CONSTANTS.HolidayEvents["Midsummer Fire Festival"],
-        coords = { 
-			{ m = 265, x = 31.2, y = 50.8, i = true, n = L["Ahune"] }
-		},
-    },
+		coords = { { m = 265, x = 31.2, y = 50.8, i = true, n = L["Ahune"] } },
+	},
 }
 
 Rarity.ItemDB.MergeItems(Rarity.ItemDB.mounts, holidayEventMountsMidnight)
