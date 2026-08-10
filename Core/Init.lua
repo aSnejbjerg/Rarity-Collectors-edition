0a4b4273ba4f9f56049264adf4a6eace5316ee66
Shelinka <89917663+Shelinka@users.noreply.github.com>
2026-08-10 22:09:28 +0200

    Version display update in options menu (#59)
    * changed version in toc to just @project-version@
    addon options should now show Rarity: Collector's Edition vX.Y.Z

--- Core/Init.lua
+++ Core/Init.lua
@@ -14,10 +14,8 @@ Rarity = LibStub("AceAddon-3.0"):NewAddon(
 	"AceSerializer-3.0"
 )
 -- Rarity.MINOR_VERSION = tonumber(("$Revision: 650 $"):match("%d+"))  -- Disabled after switching entirely to Git (from SVN)
-local projectVersion, noReplacements = (C_AddOns.GetAddOnMetadata("Rarity", "Version"):match("r%d+") or "r0"):gsub(
-	"r",
-	""
-) -- e.g., r654	1 (the second value can be discarded)
+Rarity.ADDON_VERSION = C_AddOns.GetAddOnMetadata("Rarity", "Version") or "0"
+local projectVersion, noReplacements = (Rarity.ADDON_VERSION:match("r%d+") or "r0"):gsub("r", "") -- e.g., r654	1 (the second value can be discarded)
 Rarity.MINOR_VERSION = tonumber(projectVersion)
 
 -- Set up modules
