ec8c06608f4dbaa3b5c47053d86195cf71bbe4ab
Dykataa <ogo_rasmussen@hotmail.com>
2026-08-23 14:20:30 +0200

    Fix formatting for Primal Egg entry in MistsOfPandaria

--- DB/Toys/MistsOfPandaria.lua
+++ DB/Toys/MistsOfPandaria.lua
@@ -209,7 +209,7 @@ local mopToys = {
 		chance = 6,
 		coords = { { m = 379, x = 57.6, y = 75.0, n = L["Havak"] } },
 	},
-		["Primal Egg"] = {
+	["Primal Egg"] = {
 		cat = CONSTANTS.ITEM_CATEGORIES.MOP,
 		type = CONSTANTS.ITEM_TYPES.ITEM,
 		method = CONSTANTS.DETECTION_METHODS.NPC,
